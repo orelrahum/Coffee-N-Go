88e7d99bbdc90e226cee5b19c1babfc77672550d
bargenish44 <44568486+bargenish44@users.noreply.github.com>
2019-12-02 05:11:22 +0200

    login

--- app/src/main/java/com/example/coffee_n_go/MainActivity.java
+++ app/src/main/java/com/example/coffee_n_go/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.coffee_n_go;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.worker);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openWorker();
+            }
+        });
+    }
+    public void openWorker(){
+        Intent intent = new Intent(this,Login.class);
+        startActivity(intent);
     }
 }

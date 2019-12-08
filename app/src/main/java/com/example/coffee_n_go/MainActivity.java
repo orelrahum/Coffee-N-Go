28604fbab837a36a415986c3ad858b6149798ad0
bargenish44 <44568486+bargenish44@users.noreply.github.com>
2019-12-08 19:17:15 +0200

    updates

--- app/src/main/java/com/example/coffee_n_go/MainActivity.java
+++ app/src/main/java/com/example/coffee_n_go/MainActivity.java
@@ -1,15 +1,31 @@
 package com.example.coffee_n_go;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.core.Tag;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.*;
 
 
 public class MainActivity extends AppCompatActivity {
     private Button button;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    public Map<String, Object> user = new HashMap<>();
+    private static final String TAG = "MainActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
                 openWorker();
             }
         });
+
     }
     public void openWorker(){
         Intent intent = new Intent(this,Login.class);

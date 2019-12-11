99ccbeee34da4c6fe78b8d0cb2cc31fcaa030c77
orelrahum <44848947+orelrahum@users.noreply.github.com>
2019-12-11 17:01:54 +0200

    aa

--- app/src/main/java/com/example/coffee_n_go/MainActivity.java
+++ app/src/main/java/com/example/coffee_n_go/MainActivity.java
@@ -10,7 +10,9 @@ import java.util.*;
 
 
 public class MainActivity extends AppCompatActivity {
-    private Button button;
+    private Button button_worker;
+    private Button button_CEO;
+    private Button button_customer;
     FirebaseFirestore db = FirebaseFirestore.getInstance();
     public Map<String, Object> user = new HashMap<>();
     private static final String TAG = "MainActivity";
@@ -19,14 +21,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button = (Button) findViewById(R.id.worker);
-        button.setOnClickListener(new View.OnClickListener() {
+        button_worker = (Button) findViewById(R.id.worker);
+        button_worker.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 openWorker();
             }
         });
-
+        button_customer= (Button) findViewById(R.id.customer);
+        button_customer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openWorker();
+            }
+        });
+        button_CEO= (Button) findViewById(R.id.CEO);
+        button_CEO.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openWorker();
+            }
+        });
     }
     public void openWorker(){
         Intent intent = new Intent(this,Login.class);

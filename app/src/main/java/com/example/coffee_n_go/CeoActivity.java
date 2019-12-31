02e790c1490dc7a2fd729df0668365bc012df204
bargenish44 <44568486+bargenish44@users.noreply.github.com>
2019-12-31 05:12:47 +0200

    updates

--- app/src/main/java/com/example/coffee_n_go/CeoActivity.java
+++ app/src/main/java/com/example/coffee_n_go/CeoActivity.java
@@ -7,7 +7,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import com.google.firebase.database.DatabaseReference;
 
 public class CeoActivity extends AppCompatActivity {
     Button chnagePricesBtn;

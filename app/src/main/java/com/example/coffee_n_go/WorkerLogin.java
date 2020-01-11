2d155ddfe5febd801f0435f67b2231f8aa73e58e
bargenish44 <44568486+bargenish44@users.noreply.github.com>
2020-01-11 15:22:32 +0200

    Update WorkerLogin.java

--- app/src/main/java/com/example/coffee_n_go/WorkerLogin.java
+++ app/src/main/java/com/example/coffee_n_go/WorkerLogin.java
@@ -76,7 +76,6 @@ public class WorkerLogin extends AppCompatActivity {
                         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK);
                         startActivity(intent);
                     } else if(dest.equals("ceo")) {
-                        mLoginBtn.setText(users.toString());
                         for (User user : users) {
                             if (user.getAuthID().equals(Uid)) {
                                 if (user.getPermissions().equals("MANAGER")) {
